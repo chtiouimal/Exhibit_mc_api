f4b9975524dcf71d3e9608800bf98e1aabc70842
malek.chtioui <chtiouimal@gmail.com>
2023-09-25 12:57:10 +0100

    cors fix

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const cors = require("cors")
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.use(cors())
+app.use(
+  cors({
+    origin: "*",
+  }),
+)
 app.use(express.json())
 // optional: use url form
 app.use(express.urlencoded({ extended: false }))
