33f7abee4f6fc6569195712076ab2164a51f7db3
malek.chtioui <chtiouimal@gmail.com>
2023-09-25 13:09:47 +0100

    cors fix 4

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000
 
 app.use(
   cors({
-    origin: "http://127.0.0.1:5173/",
+    origin: "http://127.0.0.1:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
   }),
 )
