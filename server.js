4ff1cd45931c3872c4a96411dc3f6cc6bab90ad4
unknown <chtiouimal@gmail.com>
2024-07-09 14:53:53 +0100

    add dotenv require

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config()
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
+require('dotenv').config
 
 const app = express()
 const PORT = process.env.PORT || 3000
