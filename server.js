1bd246b2e1dbc0d904ba176673c632e2cbb1d4e5
unknown <chtiouimal@gmail.com>
2024-08-21 14:26:36 +0100

    add cors options

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ require('dotenv').config
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.use(cors())
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions))
 app.use(express.json())
 // optional: use url form
 app.use(express.urlencoded({ extended: false }))
